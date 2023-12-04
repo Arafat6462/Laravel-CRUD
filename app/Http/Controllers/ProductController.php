4b7239d8cc942be9d1f99744715396ebfe90de42
Arafat Hossain <arafat6462@gmail.com>
2023-12-04 11:45:00 +0600

    new route with create

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
@@ -14,5 +15,25 @@ class ProductController extends Controller
     public function create()
     {
         return view('products.create');
+    }
+
+    public function store(Request $request)
+    {
+        // dd($request->all());
+        // dd($request);
+        // dd($request->name);
+
+        //validate data
+        $data = $request->validate([
+            'name' => 'required',
+            'qty' => 'required',
+            'price' => 'required',
+            'description' => 'required'
+        ]);
+
+        //store data
+        $NewProduct = Product::create($data);
+        return redirect()->route('products.index');
+
     }
 }
--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE htext>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -6,7 +6,31 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Product</h1>
-    <div>Create</div>
+    <h1>Create Product</h1>
+ 
+    <form method="post" action="{{route('products.store')}}">
+        @csrf
+        @method('post')
+        <div>
+            <label for="name">Name</label>
+            <input type="text" name="name" placeholder="name">
+        </div>
+        <div>
+            <label for="qty">Qty</label>
+            <input type="text" name="qty" placeholder="qty">
+        </div>
+        <div>
+            <label for="name">Price</label>
+            <input type="text" name="price" placeholder="price">
+        </div>
+        <div>
+            <label for="name">description</label>
+            <input type="text" name="description" placeholder="description">
+        </div>
+
+        <div>
+            <input type="submit" value="Save a New Product">
+        </div>
+    </form>
 </body>
 </html>
